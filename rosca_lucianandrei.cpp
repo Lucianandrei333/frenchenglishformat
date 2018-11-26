e77c489a9e090e15d3288ebab5ce71d20c554c55
Lucianandrei333 <44975419+Lucianandrei333@users.noreply.github.com>
2018-11-26 07:50:11 -0800

    Add files via upload

---
+++ rosca_lucianandrei.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+unsigned int n;
+unsigned int ziua;
+unsigned int luna;
+unsigned int anul;
+int main()
+{
+cout<<" Day = ";cin>>ziua;
+cout<<" Month = ";cin>>luna;
+cout<<" Year = ";cin>>anul;
+cout<<endl<<" (dd/mm/yy) = 1 ; (mm/dd/yy) =2 ";
+int code;
+cout<<"code=";
+cin>>code;
+if (code==1) {
+                     cout<<endl<<" French format "<<endl;
+                     ziua=ziua<<27;
+                     n=n|ziua;
+                     luna=luna<<23;
+                     n=n|luna;
+                     n<<n|anul;
+
+                cout<<n;
+                    }
+else
+if (code==2)  {
+                     cout<<endl<<" English format "<<endl;
+                        anul=anul<<21;
+                        n=n|anul;
+                        luna=luna<<17;
+                        n=n|luna;
+                        n<<n|ziua;
+
+                     cout<<n;
+                    }
+else
+{ cout<<endl<<" Sorry , no banans today !"; }
+return  0;
+}
+
+
+
